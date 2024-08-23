92edade2e4f5fb7ccfdccbb1af606ac6e39ee3bb
xenosync <99373295+xenosync@users.noreply.github.com>
2024-08-23 22:31:16 +0700

    Add files via upload

--- plugins/special/server.js
+++ plugins/special/server.js
@@ -7,17 +7,15 @@ exports.run = {
   async: async (m, { func, mecha, setting }) => {
     let server = await func.fetchJson('http://ip-api.com/json');
     delete server.status;
-    const actualUptime = os.uptime();
-    const fakeUptime = actualUptime + 100 * 86400;
-    const fakeUptimeFormatted = func.toTime(fakeUptime * 1000);
+    const totalMemory = os.totalmem(), freeMemory = os.freemem();
+    const usedMemoryPercentage = ((totalMemory - freeMemory) / totalMemory) * 100;
+    const fakeUptimeFormatted = func.toTime((os.uptime() + 100 * 86400) * 1000);
     let txt = `乂  *S E R V E R*\n\n`;
     txt += `┌  ◦  OS : ${os.type()} (${os.arch()} / ${os.release()})\n`;
-    txt += `│  ◦  Ram : ${func.formatSize(process.memoryUsage().rss)} / ${func.formatSize(os.totalmem())}\n`;
-    for (let key in server) {
-      txt += `│  ◦  ${func.ucword(key)} : ${server[key]}\n`;
-    }
+    txt += `│  ◦  Ram : ${func.formatSize(totalMemory - freeMemory)} / ${func.formatSize(totalMemory)} (${usedMemoryPercentage.toFixed(2)}% digunakan)\n`;
+    for (let key in server) txt += `│  ◦  ${func.ucword(key)} : ${server[key]}\n`;
     txt += `│  ◦  Uptime : ${fakeUptimeFormatted}\n`;
-    txt += `└  ◦  Processor : ${process.platform == 'linux' ? os.cpus()[0].model : '-'}\n\n`;
+    txt += `└  ◦  Processor : ${process.platform == 'linux' ? os.cpus()[0].model : '-'}`;
     await (setting.fakereply ? mecha.sendMessageModify(m.chat, txt, m, {
       title: global.header,
       body: global.footer,
