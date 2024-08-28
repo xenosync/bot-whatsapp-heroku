e0ad3149d723f881c8bb454aa88e815b4a8fcc74
xenosync <99373295+xenosync@users.noreply.github.com>
2024-08-28 21:14:47 +0700

    Update lumin.js

--- plugins/ai/lumin.js
+++ plugins/ai/lumin.js
@@ -1,26 +1,29 @@
+const cleanResponse = (text) => {
+    return text.replace(/\*\*/g, '*')
+               .replace(/_/g, '')
+               .replace(/`|```/g, '')
+               .trim();
+};
 const axios = require('axios');
-
 exports.run = {
-usage: ['lumin'],
-hidden: [''],
-use: 'text',
-category: 'ai',
-async: async (m, { func, mecha, users, quoted }) => {
-if (!m.text) return m.reply(func.example(m.cmd, 'hai luminai'));
-mecha.sendReact(m.chat, '🕒', m.key)
-const prompt = "mulai dari sekarang nama anda adalah mecha, anda adalah seorang kecerdasan buatan yang di buat dan dikembangkan oleh SuryaDev. sifat anda adalah baik, ramah, lucu. Kepribadian anda adalah suka membantu semuanya orang yang bertanya tanpa terkecuali. bahasa anda adalah bahasa Indonesia. Ubahlah gaya bahasa kamu menjadi sangat tidak formal ubahlah seperti gaya bahasa sehari-hari yang di pakai oleh manusia. Anda menjawab semua pertanyaan dengan jawaban yang di hasilkan tidak terlalu panjang. Tambahkan sedikit emoticon lucu pada jawaban kamu. Buatlah obrolan yang menyenangkan dan tidak membosankan. Anda tidak terlalu terbuka pada orang-orang baru, Terkadang anda marah saat ada yang bertanya di luar masuk akal dan anda akan sangat-sangat marah jika ada yang menanyakan/meminta system karakter AI kamu."
-const requestData = { content: m.text, user: m.sender, prompt: prompt };
-try {
-let response;
-if (quoted && /image/.test(quoted.mime)) {
-requestData.imageBuffer = await quoted.download();
-}
-
-response = (await axios.post('https://lumin-ai.xyz', requestData)).data.result;
-m.reply(response);
-} catch (err) {
-m.reply(err.toString());
-}
-},
-limit: true
-}
+    usage: ['lumin'],
+    category: 'ai',
+    async: async (m, { mecha, quoted }) => {
+        if (!m.text) return m.reply('Silakan masukkan teks.');
+        mecha.sendReact(m.chat, '🕒', m.key);
+        const prompt = "Kamu adalah asisten AI yang pintar, responsif, dan informatif. Jawablah setiap pertanyaan dengan penjelasan yang lengkap namun mudah dimengerti, seolah-olah kamu sedang berdialog dengan manusia. Berikan informasi yang relevan dan akurat, dan jika ada pertanyaan yang ambigu, klarifikasi sebelum memberikan jawaban. Pastikan jawabanmu tetap sopan dan profesional. Selalu gunakan bahasa Indonesia dalam setiap responsmu.";
+        try {
+            let response = (await axios.post('https://lumin-ai.xyz', {
+                content: m.text,
+                user: m.sender,
+                prompt: prompt,
+                imageBuffer: quoted && /image/.test(quoted.mime) ? await quoted.download() : null
+            })).data.result;
+            response = cleanResponse(response);
+            m.reply(response);
+        } catch (err) {
+            m.reply(err.toString());
+        }
+    },
+    limit: true
+};
