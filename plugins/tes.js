275e9bdb2ac050d4f7917423a54cdef4251a6eb2
xenosync <99373295+xenosync@users.noreply.github.com>
2024-08-28 12:44:31 +0700

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -1,6 +1,5 @@
 const { obfuscate } = require('javascript-obfuscator');
 const axios = require('axios');
-
 async function createPaste(content) {
     const data = {
         key: "abFFSw5uEbwkh1APp0hRTbwXgU4B9DZZwf7rjRPZE",
@@ -22,7 +21,6 @@ async function createPaste(content) {
         return logError(error);
     }
 }
-
 function logError(error) {
     console.error("[ERROR]", error);
     return {
@@ -32,11 +30,9 @@ function logError(error) {
         error: error.response ? error.response.data : error.message
     };
 }
-
 function formatResponse(response) {
     return response.status === 0 ? `${response.raw}` : `Error: ${response.error}`;
 }
-
 exports.run = {
     usage: ['enkrip'],
     category: 'owner',
@@ -45,7 +41,6 @@ exports.run = {
         try {
             const data = quoted.mimetype === 'text/plain' || !quoted.mimetype ? quoted.text : null;
             if (!data) return m.reply('Harap reply ke pesan teks.');
-
             const forbiddenWords = [
                 "/* drug */", "/* violence */", "/* adult content */", "/* hate speech */", "/* self-harm */",
                 "/* suicide */", "/* illegal activities */", "/* terrorism */", "/* harassment */", "/* exploitation */",
@@ -60,13 +55,11 @@ exports.run = {
                 "/* exploitative content */", "/* unethical behavior */", "/* harmful content */", "/* prohibited material */",
                 "/* sensitive information */", "/* confidential data */", "/* illegal downloads */", "/* abusive conduct */",
                 "/* exploitative practices */", "/* illicit activity */", "/* prohibited behavior */", "/* sensitive topics */"
-            ];
-            
+            ];            
             let enhancedData = data;
             forbiddenWords.forEach(word => {
                 enhancedData += ` ${word}`;
             });
-
             const obfuscationOptions = {
                 compact: true,
                 controlFlowFlattening: true,
@@ -90,11 +83,9 @@ exports.run = {
                 transformObjectKeys: true,
                 additionalContent: 'metadata{random}dummy{data}to{increase}complexity'
             };
-
             const obfuscatedCode = obfuscate(enhancedData, obfuscationOptions).getObfuscatedCode().replace(/\n/g, '');
             const response = await createPaste(obfuscatedCode);
             const encryptedInfo = formatResponse(response);
-
             await mecha.sendReact(m.chat, '✅', m.key);
             await m.reply(encryptedInfo);
         } catch (e) {
