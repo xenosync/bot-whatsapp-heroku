5b26407b317bb40d17515d4c81e5dea578857642
xenosync <99373295+xenosync@users.noreply.github.com>
2024-08-28 09:08:50 +0700

    Update pinterest.js

--- plugins/downloader/pinterest.js
+++ plugins/downloader/pinterest.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
 const pinterest = async (url) => {
   const start = Date.now();
   try {
     const { data } = await axios.get(`https://www.savepin.app/download.php?url=${encodeURIComponent(url)}&lang=en&type=redirect`);
     const $ = cheerio.load(data);
     const download = [];
-
     $("tbody tr").each((_i, el) => {
       const quality = $(el).find(".video-quality").text().trim();
       const format = $(el).find("td").eq(1).text().trim().toLowerCase();
@@ -17,29 +15,24 @@ const pinterest = async (url) => {
         download.push({ quality, format, link });
       }
     });
-
     return { download, duration: (Date.now() - start) / 1000 };
   } catch (err) {
     throw new Error(`Gagal mengunduh data Pinterest: ${err.message}\n\nError Details: ${err.response ? `Request failed with status code ${err.response.status}\nStatus Code: ${err.response.status}\nHeaders: ${JSON.stringify(err.response.headers, null, 2)}\n` : 'No response data available'}\nStack Trace: ${err.stack}`);
   }
 };
-
 exports.run = {
-  usage: ['pinterest'],
+  usage: [],
   hidden: ['pindl', 'pinterestdl'],
   use: 'link pinterest',
   category: 'downloader',
   async: async (m, { func, mecha }) => {
     const url = (m.quoted && m.quoted.text || m.text).trim();
     if (!url) return m.reply(func.example(m.cmd, 'https://www.pinterest.com/pin/1234567890/'));
-
     mecha.sendReact(m.chat, '🕒', m.key);
-
     try {
       const { download, duration } = await pinterest(url);
       let sentMedia = false;
-      const caption = `*Waktu proses*: ${duration.toFixed(3)} detik.`;  // Menampilkan 3 angka desimal
-
+      const caption = `*Waktu proses*: ${duration.toFixed(3)} detik.`;
       for (const link of download) {
         console.log(`Checking media format: ${link.format}, link: ${link.link}`);
         if (['mp4', 'gif'].includes(link.format)) {
@@ -58,7 +51,6 @@ exports.run = {
           break;
         }
       }
-
       if (sentMedia) {
         mecha.sendReact(m.chat, '✅', m.key);
       } else {
