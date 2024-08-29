3a6f3a888590a9b79fdc053074051df4f2bd0848
xenosync <99373295+xenosync@users.noreply.github.com>
2024-08-29 20:35:07 +0700

    Update checkip.js

--- plugins/tools/checkip.js
+++ plugins/tools/checkip.js
@@ -24,10 +24,9 @@ exports.run = {
     async: async (m, { func, mecha }) => {
         if (!m.text) return m.reply(`Masukkan IP Address!\n\n*Contoh:* ${m.cmd} 64.227.103.212`);
         if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(m.text)) return m.reply('Invalid IP Address format.');
-        
         mecha.sendReact(m.chat, '🕒', m.key);
         let anu = await checkIP(m.text.trim());
-        m.reply(`*Hasil Check IP ${m.text} :*\n\n${anu}`);
+        m.reply(`${anu}`);
     },
     limit: true
 }
